8f133b95979ac1274dcfdfcd7c952bf6c3976959
niltor <zpty@outlook.com>
2022-10-27 18:11:38 +0800

    fix path

--- src/CommandLine/Commands/ConfigCommand.cs
+++ src/CommandLine/Commands/ConfigCommand.cs
@@ -11,22 +11,11 @@ public class ConfigCommand
     /// </summary>
     public static async Task InitConfigFileAsync()
     {
-        var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
-        var solutionPath = AssemblyHelper.GetSlnFile(currentDir, "*.sln", currentDir.Root);
+        var configPath = GetConfigPath();
 
-        string configPath;
-        if (solutionPath == null)
-        {
-            Console.WriteLine("can't find sln file, use system user dir");
-            configPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        }
-        else
-        {
-            configPath = solutionPath.FullName;
-        }
         var file = new FileInfo(Path.Combine(configPath, Config.ConfigFileName));
         var path = file == null
-            ? Path.Combine(solutionPath.DirectoryName!, Config.ConfigFileName)
+            ? Path.Combine(configPath, Config.ConfigFileName)
             : file.FullName;
 
         if (File.Exists(path))
@@ -44,46 +33,23 @@ public class ConfigCommand
     /// </summary>
     public static ConfigOptions? ReadConfigFile()
     {
-        var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
-        var solutionPath = AssemblyHelper.GetSlnFile(currentDir, "*.sln", currentDir.Root);
-        string configPath;
-        if (solutionPath == null)
-        {
-            Console.WriteLine("can't find sln file, use system user dir");
-            configPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        }
-        else
-        {
-            configPath = solutionPath.FullName;
-        }
+        var configPath = GetConfigPath();
         var file = new FileInfo(Path.Combine(configPath, Config.ConfigFileName));
         if (file == null)
         {
             Console.WriteLine($"config file not found , please run droplet confing init");
             return default;
         }
-        var path =  file.FullName;
+        var path = file.FullName;
         var config = File.ReadAllText(path);
         var options = JsonSerializer.Deserialize<ConfigOptions>(config);
         return options ?? new ConfigOptions();
     }
 
-
     public static void EditConfigFile()
     {
-        var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
-        var solutionPath = AssemblyHelper.GetSlnFile(currentDir, "*.sln", currentDir.Root);
-        string configPath;
-        if (solutionPath == null)
-        {
-            Console.WriteLine("can't find sln file, use system user dir");
-            configPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        }
-        else
-        {
-            configPath = solutionPath.FullName;
-        }
-        var file = new FileInfo(Path.Combine(configPath,Config.ConfigFileName));
+        var configPath = GetConfigPath();
+        var file = new FileInfo(Path.Combine(configPath, Config.ConfigFileName));
         if (file == null)
         {
             Console.WriteLine($"config file not found , please run droplet confing init");
@@ -104,5 +70,26 @@ public class ConfigCommand
         process.Start();
         _ = process.StandardOutput.ReadToEnd();
         process.WaitForExit();
+    }
+
+    /// <summary>
+    /// 获取config目录路径，优先为sln解决方案目录，如果没有保存到LocalApplicationData
+    /// </summary>
+    /// <returns></returns>
+    private static string GetConfigPath()
+    {
+        var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
+        var solutionPath = AssemblyHelper.GetSlnFile(currentDir, "*.sln", currentDir.Root);
+        string configPath;
+        if (solutionPath == null)
+        {
+            Console.WriteLine("can't find sln file, use system user dir");
+            configPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        }
+        else
+        {
+            configPath = solutionPath.Directory.FullName;
+        }
+        return configPath;
     }
 }
