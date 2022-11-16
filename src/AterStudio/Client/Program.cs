5f676a18d6afc9bdc40791909d46c6ebd793aa23
niltor <zpty@outlook.com>
2022-11-16 18:46:25 +0800

    temp save

--- src/AterStudio/Client/Program.cs
+++ src/AterStudio/Client/Program.cs
@@ -1,4 +1,7 @@
 using AterStudio.Client;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,4 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services
+    .AddBlazorise(options =>
+    {
+      options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 await builder.Build().RunAsync();
--- src/AterStudio/Shared/Models/AddProjectForm.cs
+++ src/AterStudio/Shared/Models/AddProjectForm.cs
@@ -1,7 +1,11 @@
-﻿namespace AterStudio.Shared.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AterStudio.Shared.Models;
 
 public record AddProjectForm
 {
+  [Required]
   public required string DisplayName { get; set; }
+  [Required]
   public required string Path { get; set; }
 }
