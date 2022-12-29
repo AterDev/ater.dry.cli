49b179515a4eb7748e545e8ce94b33bba75bce38
niltor <zpty@outlook.com>
2022-12-29 18:34:44 +0800

    update

--- src/AterStudio/ClientApp/src/app/pages/workspace/docs/docs.component.ts
+++ src/AterStudio/ClientApp/src/app/pages/workspace/docs/docs.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocsComponent implements OnInit {
 
+  isRefresh = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+  refresh(): void { }
 }
