e128a4b821e6ee0d4737dfd341a6099ba463cd53
niltor <zpty@outlook.com>
2023-02-08 00:04:13 +0800

    update

--- src/CodeGenerator/Generate/ProtobufGenerate.cs
+++ src/CodeGenerator/Generate/ProtobufGenerate.cs
@@ -39,7 +39,7 @@ service {EntityInfo.Name} {{
     }
 
     /// <summary>
-    /// C#属性转proto 字段
+    /// C#属性转proto字段
     /// </summary>
     /// <param name="property"></param>
     /// <param name="sort">默认排序值 </param>
@@ -54,45 +54,96 @@ service {EntityInfo.Name} {{
     }
 
     /// <summary>
-    /// 生成实体模型对应的message
+    /// 构建message
     /// </summary>
+    /// <param name="name"></param>
+    /// <param name="fields"></param>
     /// <returns></returns>
-    public string GenerateMessages()
+    internal static string BuildMessage(string name, string fields)
     {
         var sb = new StringBuilder();
-        sb.Append("message ").Append(EntityInfo.Name);
+        sb.Append("message ").Append(name);
         sb.Append(Environment.NewLine);
         sb.Append('{');
         sb.Append(Environment.NewLine);
-        var fields = "";
+        sb.Append(fields);
+        sb.Append('}');
+        return sb.ToString();
+    }
 
+    /// <summary>
+    /// 生成实体模型对应的message
+    /// </summary>
+    /// <returns></returns>
+    public string GenerateEntityMessage()
+    {
+        var fields = "";
         // 选择合适的属性
         for (int i = 0; i < EntityInfo.PropertyInfos.Count; i++)
         {
             var prop = EntityInfo.PropertyInfos[i];
             fields += ToProtobufField(prop, i + 1);
         }
-
-        sb.Append('}');
-        return string.Empty;
+        return BuildMessage(EntityInfo.Name, fields);
     }
 
     public string GenerateFilterMessage()
     {
-        return string.Empty;
+        var fields = "";
+        string[] filterFields = new string[] { "Id", "CreatedTime", "UpdatedTime", "IsDeleted", "PageSize", "PageIndex" };
+        var properties = EntityInfo.PropertyInfos?
+            .Where(p => (p.IsRequired && !p.IsNavigation)
+                || (!p.IsList
+                    && !p.IsNavigation
+                    && !filterFields.Contains(p.Name)
+                    || p.IsEnum)
+                )
+            .Where(p => p.MaxLength is not (not null and >= 1000))
+            .ToList();
+        for (int i = 0; i < properties?.Count; i++)
+        {
+            var prop = properties[i];
+            fields += ToProtobufField(prop, i + 1);
+        }
+        return BuildMessage("FilterDto", fields);
     }
 
     public string GenerateAddMessage()
     {
-        return string.Empty;
+        var fields = "";
+        var properties = EntityInfo.PropertyInfos?.Where(p => p.Name is not "Id"
+                and not "CreatedTime"
+                and not "UpdatedTime"
+                and not "IsDeleted")
+            .ToList();
 
+        for (int i = 0; i < properties?.Count; i++)
+        {
+            var prop = properties[i];
+            fields += ToProtobufField(prop, i + 1);
+        }
+        return BuildMessage("AddDto", fields);
     }
 
     public string GenerateUpdateMessage()
     {
-        return string.Empty;
+        var fields = "";
+        var properties = EntityInfo.PropertyInfos?.Where(p => p.Name is not "Id"
+                and not "CreatedTime"
+                and not "UpdatedTime"
+                and not "IsDeleted")
+            .ToList();
+
+        for (int i = 0; i < properties?.Count; i++)
+        {
+            var prop = properties[i];
+            prop.IsNullable = true;
+            fields += ToProtobufField(prop, i + 1);
+        }
+        return BuildMessage("AddDto", fields);
 
     }
+
     public string GenerateIdMessage()
     {
         return string.Empty;
@@ -104,11 +155,7 @@ service {EntityInfo.Name} {{
         return string.Empty;
 
     }
-    public string GenerateEntityMessage()
-    {
-        return string.Empty;
 
-    }
     public string GenerateListMessage()
     {
         return string.Empty;
