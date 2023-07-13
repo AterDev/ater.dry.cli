e4a2617fa4d0a3b8be71e659bfb491e24b41601d
niltor <zpty@outlook.com>
2023-07-13 18:26:54 +0800

    update solutionHelper

--- src/Core/Infrastructure/Helper/SolutionHelper.cs
+++ src/Core/Infrastructure/Helper/SolutionHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.MSBuild;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
 
 namespace Core.Infrastructure.Helper;
 /// <summary>
@@ -46,20 +47,58 @@ public class SolutionHelper : IDisposable
         _ = Workspace.CurrentSolution.AddProjectReference(currentProject.Id, new ProjectReference(referenceProject.Id));
     }
 
-
+    /// <summary>
+    /// 重命名Namesapce
+    /// </summary>
+    /// <param name="oldName"></param>
+    /// <param name="newName"></param>
+    /// <param name="projectName"></param>
     public void RenameNamespace(string oldName, string newName, string? projectName = null)
     {
-
+        var projects = Solution.Projects;
+        if (projectName != null)
+        {
+            projects = projects.Where(p => p.Name == projectName);
+        }
+        Parallel.ForEach(projects, p =>
+        {
+            Parallel.ForEach(p.Documents, d =>
+            {
+                var root = d.GetSyntaxRootAsync().Result;
+                var newRoot = root.ReplaceNodes(root.DescendantNodes().OfType<NamespaceDeclarationSyntax>(), (oldNode, newNode) =>
+                {
+                    var node = (NamespaceDeclarationSyntax)oldNode;
+                    if (node.Name.ToString() == oldName)
+                    {
+                        return node.WithName(SyntaxFactory.ParseName(newName));
+                    }
+                    return node;
+                });
+                var newDocument = d.WithSyntaxRoot(newRoot);
+                Workspace.TryApplyChanges(newDocument.Project.Solution);
+            });
+        });
     }
 
-    public void RemoveProject(string projectPath)
+    /// <summary>
+    /// 从解决方案中移除项目
+    /// </summary>
+    /// <param name="projectPath"></param>
+    public async void RemoveProject(string projectPath)
     {
-
+        var file = new FileInfo(projectPath);
+        var project = await Workspace.OpenProjectAsync(file.FullName);
+        _ = Workspace.CurrentSolution.RemoveProject(project.Id);
     }
 
+    /// <summary>
+    /// 删除项目引用
+    /// </summary>
+    /// <param name="currentProject"></param>
+    /// <param name="referenceProjectPath"></param>
     public void RemoveProjectReference(Project currentProject, Project referenceProjectPath)
     {
-
+        _ = Workspace.CurrentSolution.RemoveProjectReference(currentProject.Id, new ProjectReference(referenceProjectPath.Id));
     }
 
 
