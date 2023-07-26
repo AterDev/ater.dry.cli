8aef38eeac7d7fd00d18331ce939458a5eae5a6a
niltor <zpty@outlook.com>
2023-07-26 17:45:07 +0800

    add console logo

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -8,8 +8,30 @@ internal class Program
     private static async Task<int> Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
+        if (args.Length == 0)
+        {
+            return 0;
+        }
+
+        ShowLogo();
         await ConfigCommand.InitConfigFileAsync();
         RootCommand root = new CommandBuilder().Build();
         return await root.InvokeAsync(args);
+    }
+
+    private static void ShowLogo()
+    {
+        var logo = """
+             _____    _____   __     __
+            |  __ \  |  __ \  \ \   / /
+            | |  | | | |__) |  \ \_/ / 
+            | |  | | |  _  /    \   /  
+            | |__| | | | \ \     | |   
+            |_____/  |_|  \_\    |_|
+
+                     -- for freedom --
+            """;
+
+        Console.WriteLine(logo);
     }
 }
