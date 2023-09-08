b88b582447f1d6157a019efa39c3581eec4a9a4e
niltor <zpty@outlook.com>
2023-09-08 11:22:40 +0800

    enum pipe  injectable

--- src/CodeGenerator/Templates/angular/enum.pipe.ts
+++ src/CodeGenerator/Templates/angular/enum.pipe.ts
@@ -1,8 +1,11 @@
 // 该文件自动生成，会被覆盖更新
-import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'enumText'
+})
+@Injectable({
+  providedIn: 'root'
 })
 export class EnumTextPipe implements PipeTransform {
   transform(value: unknown, type: string): string {
