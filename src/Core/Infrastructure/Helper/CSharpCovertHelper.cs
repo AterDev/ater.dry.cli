b680813d45dddf47b9040fb47572fbd8e6e5d7d4
niltor <zpty@outlook.com>
2023-12-12 17:47:05 +0800

    完成日期类型的判断

--- src/Core/Infrastructure/Helper/CSharpCovertHelper.cs
+++ src/Core/Infrastructure/Helper/CSharpCovertHelper.cs
@@ -48,51 +48,101 @@ public class CSharpCovertHelper
         {
             var propertyName = property.Name;
             var propertyValue = property.Value;
-
-            string csharpValue = propertyValue.ValueKind switch
+            var defaultValue = "";
+            // 类型处理
+            string csharpType = propertyValue.ValueKind switch
             {
                 JsonValueKind.Number => "int",
-                JsonValueKind.String => "required string",
+                JsonValueKind.String => "string?",
                 JsonValueKind.True => "bool",
                 JsonValueKind.False => "bool",
                 JsonValueKind.Object => propertyName.ToPascalCase(),
-                JsonValueKind.Array => $"List<{propertyName.ToPascalCase()}>?",
+                JsonValueKind.Array => $"List<{propertyName.ToPascalCase()}>",
                 JsonValueKind.Null => "object?",
                 _ => "object",
             };
             if (propertyValue.ValueKind == JsonValueKind.Number)
             {
+                if (propertyValue.TryGetDouble(out _))
+                {
+                    csharpType = "double";
+                }
 
-                if (propertyValue.TryGetDouble(out var doubleValue))
+                if (propertyValue.TryGetInt32(out _))
                 {
-                    csharpValue = "double";
+                    csharpType = "int";
                 }
-                if (propertyValue.TryGetInt32(out var intValue))
+
+                if (propertyValue.TryGetInt64(out _))
                 {
-                    csharpValue = "int";
-                }
-                if (propertyValue.TryGetInt64(out var longValue))
-                {
-                    csharpValue = "long";
+                    csharpType = "long";
                 }
             }
             if (propertyValue.ValueKind == JsonValueKind.String)
             {
-                if (propertyValue.TryGetGuid(out var stringValue))
+                if (propertyValue.TryGetGuid(out _))
                 {
-                    csharpValue = "Guid";
+                    csharpType = "Guid";
                 }
+                if (propertyValue.TryGetDateTime(out _))
+                {
+                    csharpType = "DateTime";
+                }
+                if (propertyValue.TryGetDateTimeOffset(out DateTimeOffset dateTimeOffset))
+                {
+                    csharpType = "DateTimeOffset";
+                    // if this is a DateOnly
+                    if (dateTimeOffset.TimeOfDay == TimeSpan.Zero)
+                    {
+                        csharpType = "DateOnly";
+                    }
+                }
+                var stringValue = propertyValue.GetString();
+                string[] formats = ["HH:mm:ss", "HH:mm"];
+                if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
+                {
+                    csharpType = "TimeOnly";
+                }
+                formats = ["yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss"];
+                if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
+                {
+                    csharpType = "DateTime";
+                }
+
+            }
+            if (propertyValue.ValueKind == JsonValueKind.Array)
+            {
+                defaultValue = "[]";
             }
 
             if (propertyName.ToUpperFirst() != propertyName.ToPascalCase())
             {
                 sb.AppendLine($"    [JsonPropertyName(\"{propertyName}\")]");
             }
-            sb.AppendLine($"    public {csharpValue} {propertyName.ToPascalCase()} {{ get; set; }}");
+
+            // 处理非法类型或变量名称
+            if (int.TryParse(csharpType, out _))
+            {
+                csharpType = "The" + csharpType;
+            }
+            if (int.TryParse(propertyName, out _))
+            {
+                propertyName = "The" + propertyName;
+            }
+
+            var propertyLine = $"public {csharpType} {propertyName.ToPascalCase()} {{ get; set; }}";
+            if (!string.IsNullOrEmpty(defaultValue))
+            {
+                propertyLine += $" = {defaultValue};";
+            }
+            sb.AppendLine($"    {propertyLine}");
+
+            // 对象处理
             if (propertyValue.ValueKind == JsonValueKind.Object)
             {
                 GenerateClass(propertyValue, propertyName);
             }
+            // 数组处理
             else if (propertyValue.ValueKind == JsonValueKind.Array && propertyValue.GetArrayLength() > 0
                 && propertyValue[0].ValueKind == JsonValueKind.Object)
             {
@@ -101,5 +151,24 @@ public class CSharpCovertHelper
         }
         sb.AppendLine("}");
         ClassCodes.Add(sb.ToString());
+    }
+}
+/// <summary>
+/// json 解析的元信息
+/// </summary>
+internal class JsonMetadata
+{
+    public List<JsonMetadata> Descents { get; set; } = [];
+
+    public JsonMetadata? Parent { get; set; }
+
+    public JsonMetadataType Type { get; set; }
+
+    public enum JsonMetadataType
+    {
+        Object,
+        Array,
+        Dictionary,
+        KeyValue
     }
 }
