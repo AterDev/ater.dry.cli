5ebf6a1dc536b74f5271d4142769e8afabb310f5
niltor <zpty@outlook.com>
2023-12-13 15:01:23 +0800

    暂存json处理

--- src/Core/Infrastructure/Helper/CSharpCovertHelper.cs
+++ src/Core/Infrastructure/Helper/CSharpCovertHelper.cs
@@ -7,7 +7,7 @@ namespace Core.Infrastructure.Helper;
 public class CSharpCovertHelper
 {
     public List<string> ClassCodes { get; set; } = [];
-
+    public List<JsonMetadata> JsonMetadataList { get; } = [];
     /// <summary>
     /// 判断是否为合法的 json
     /// </summary>
@@ -49,67 +49,8 @@ public class CSharpCovertHelper
             var propertyName = property.Name;
             var propertyValue = property.Value;
             var defaultValue = "";
-            // 类型处理
-            string csharpType = propertyValue.ValueKind switch
-            {
-                JsonValueKind.Number => "int",
-                JsonValueKind.String => "string?",
-                JsonValueKind.True => "bool",
-                JsonValueKind.False => "bool",
-                JsonValueKind.Object => propertyName.ToPascalCase(),
-                JsonValueKind.Array => $"List<{propertyName.ToPascalCase()}>",
-                JsonValueKind.Null => "object?",
-                _ => "object",
-            };
-            if (propertyValue.ValueKind == JsonValueKind.Number)
-            {
-                if (propertyValue.TryGetDouble(out _))
-                {
-                    csharpType = "double";
-                }
 
-                if (propertyValue.TryGetInt32(out _))
-                {
-                    csharpType = "int";
-                }
-
-                if (propertyValue.TryGetInt64(out _))
-                {
-                    csharpType = "long";
-                }
-            }
-            if (propertyValue.ValueKind == JsonValueKind.String)
-            {
-                if (propertyValue.TryGetGuid(out _))
-                {
-                    csharpType = "Guid";
-                }
-                if (propertyValue.TryGetDateTime(out _))
-                {
-                    csharpType = "DateTime";
-                }
-                if (propertyValue.TryGetDateTimeOffset(out DateTimeOffset dateTimeOffset))
-                {
-                    csharpType = "DateTimeOffset";
-                    // if this is a DateOnly
-                    if (dateTimeOffset.TimeOfDay == TimeSpan.Zero)
-                    {
-                        csharpType = "DateOnly";
-                    }
-                }
-                var stringValue = propertyValue.GetString();
-                string[] formats = ["HH:mm:ss", "HH:mm"];
-                if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
-                {
-                    csharpType = "TimeOnly";
-                }
-                formats = ["yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss"];
-                if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
-                {
-                    csharpType = "DateTime";
-                }
-
-            }
+            var csharpType = ConvertToType(propertyValue, propertyName);
             if (propertyValue.ValueKind == JsonValueKind.Array)
             {
                 defaultValue = "[]";
@@ -152,15 +93,151 @@ public class CSharpCovertHelper
         sb.AppendLine("}");
         ClassCodes.Add(sb.ToString());
     }
+
+
+    public void ToCsharpClassContent(JsonElement jsonElement)
+    {
+        ConvertJsonToMetadata(jsonElement);
+        // TODO:合并处理
+        // TODO:生成类
+    }
+
+    public JsonMetadata ConvertJsonToMetadata(JsonElement jsonElement, string? rootName = "Model")
+    {
+        JsonMetadata metadata = new()
+        {
+            Name = rootName
+        };
+
+        if (jsonElement.ValueKind == JsonValueKind.Object)
+        {
+            metadata.Type = JsonMetadata.JsonMetadataType.Object;
+            metadata.Descents = [];
+
+            foreach (JsonProperty property in jsonElement.EnumerateObject())
+            {
+                var child = ConvertJsonToMetadata(property.Value, property.Name.ToPascalCase());
+                child.Parent = metadata;
+                metadata.Descents.Add(child);
+            }
+            JsonMetadataList.Add(metadata);
+        }
+        else if (jsonElement.ValueKind == JsonValueKind.Array)
+        {
+            metadata.Type = JsonMetadata.JsonMetadataType.Array;
+            if (jsonElement.GetArrayLength() > 0)
+            {
+                JsonElement firstChild = jsonElement.EnumerateArray().First();
+                var child = ConvertJsonToMetadata(firstChild, metadata.Name);
+                child.Parent = metadata;
+                metadata.Descents.Add(child);
+            }
+        }
+        else
+        {
+            metadata.Type = JsonMetadata.JsonMetadataType.KeyValue;
+            metadata.ValueType = ConvertToType(jsonElement);
+        }
+
+        return metadata;
+    }
+
+    private string ConvertToType(JsonElement element, string? propertyName = null)
+    {
+        // 类型处理
+        var csharpType = element.ValueKind switch
+        {
+            JsonValueKind.Number => "int",
+            JsonValueKind.String => "string?",
+            JsonValueKind.True => "bool",
+            JsonValueKind.False => "bool",
+            JsonValueKind.Object => propertyName?.ToPascalCase(),
+            JsonValueKind.Array => $"List<{propertyName?.ToPascalCase()}>",
+            JsonValueKind.Null => "object?",
+            _ => "object",
+        };
+        if (element.ValueKind == JsonValueKind.Number)
+        {
+            if (element.TryGetDouble(out _))
+            {
+                csharpType = "double";
+            }
+
+            if (element.TryGetInt32(out _))
+            {
+                csharpType = "int";
+            }
+
+            if (element.TryGetInt64(out _))
+            {
+                csharpType = "long";
+            }
+        }
+        if (element.ValueKind == JsonValueKind.String)
+        {
+            if (element.TryGetGuid(out _))
+            {
+                csharpType = "Guid";
+            }
+            if (element.TryGetDateTime(out _))
+            {
+                csharpType = "DateTime";
+            }
+            if (element.TryGetDateTimeOffset(out DateTimeOffset dateTimeOffset))
+            {
+                csharpType = "DateTimeOffset";
+                // if this is a DateOnly
+                if (dateTimeOffset.TimeOfDay == TimeSpan.Zero)
+                {
+                    csharpType = "DateOnly";
+                }
+            }
+            var stringValue = element.GetString();
+            string[] formats = ["HH:mm:ss", "HH:mm"];
+            if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
+            {
+                csharpType = "TimeOnly";
+            }
+            formats = ["yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss"];
+            if (DateTime.TryParseExact(stringValue, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out _))
+            {
+                csharpType = "DateTime";
+            }
+        }
+        return csharpType;
+    }
+
+
 }
 /// <summary>
 /// json 解析的元信息
 /// </summary>
-internal class JsonMetadata
+public class JsonMetadata
 {
+    /// <summary>
+    /// 键名称
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// 子级元素
+    /// </summary>
     public List<JsonMetadata> Descents { get; set; } = [];
 
+    /// <summary>
+    /// 父属性
+    /// </summary>
     public JsonMetadata? Parent { get; set; }
+
+    /// <summary>
+    /// 值类型
+    /// </summary>
+    public string? ValueType { get; set; }
+
+    /// <summary>
+    /// 可空
+    /// </summary>
+    public bool Nullable { get; set; }
 
     public JsonMetadataType Type { get; set; }
 
--- test/CodeGenerator.Test/FunctionTest.cs
+++ test/CodeGenerator.Test/FunctionTest.cs
@@ -189,13 +189,60 @@ public class FunctionTest
     [Fact]
     public void Should_ConvertJsonToCsharp()
     {
-        var json = "[{\"word\":\"hello\",\"phonetics\":[{\"audio\":\"https://api.dictionaryapi.dev/media/pronunciations/en/hello-au.mp3\",\"sourceUrl\":\"https://commons.wikimedia.org/w/index.php?curid=75797336\",\"license\":{\"name\":\"BY-SA 4.0\",\"url\":\"https://creativecommons.org/licenses/by-sa/4.0\"}},{\"text\":\"/həˈləʊ/\",\"audio\":\"https://api.dictionaryapi.dev/media/pronunciations/en/hello-uk.mp3\",\"sourceUrl\":\"https://commons.wikimedia.org/w/index.php?curid=9021983\",\"license\":{\"name\":\"BY 3.0 US\",\"url\":\"https://creativecommons.org/licenses/by/3.0/us\"}},{\"text\":\"/həˈloʊ/\",\"audio\":\"\"}],\"meanings\":[{\"partOfSpeech\":\"noun\",\"definitions\":[{\"definition\":\"\\\"Hello!\\\" or an equivalent greeting.\",\"synonyms\":[],\"antonyms\":[]}],\"synonyms\":[\"greeting\"],\"antonyms\":[]},{\"partOfSpeech\":\"verb\",\"definitions\":[{\"definition\":\"To greet with \\\"hello\\\".\",\"synonyms\":[],\"antonyms\":[]}],\"synonyms\":[],\"antonyms\":[]},{\"partOfSpeech\":\"interjection\",\"definitions\":[{\"definition\":\"A greeting (salutation) said when meeting someone or acknowledging someone’s arrival or presence.\",\"synonyms\":[],\"antonyms\":[],\"example\":\"Hello, everyone.\"},{\"definition\":\"A greeting used when answering the telephone.\",\"synonyms\":[],\"antonyms\":[],\"example\":\"Hello? How may I help you?\"},{\"definition\":\"A call for response if it is not clear if anyone is present or listening, or if a telephone conversation may have been disconnected.\",\"synonyms\":[],\"antonyms\":[],\"example\":\"Hello? Is anyone there?\"},{\"definition\":\"Used sarcastically to imply that the person addressed or referred to has done something the speaker or writer considers to be foolish.\",\"synonyms\":[],\"antonyms\":[],\"example\":\"You just tried to start your car with your cell phone. Hello?\"},{\"definition\":\"An expression of puzzlement or discovery.\",\"synonyms\":[],\"antonyms\":[],\"example\":\"Hello! What’s going on here?\"}],\"synonyms\":[],\"antonyms\":[\"bye\",\"goodbye\"]}],\"license\":{\"name\":\"CC BY-SA 3.0\",\"url\":\"https://creativecommons.org/licenses/by-sa/3.0\"},\"sourceUrls\":[\"https://en.wiktionary.org/wiki/hello\"]}]";
+        var json = """
+            {
+              "description": {
+                "title": "Contiguous U.S., Average Temperature",
+                "units": "Degrees Fahrenheit",
+                "base_period": "1901-2000",
+                "time":"12:00:12",
+                "date":"2013-12-12",
+                "datetime":"2022-12-12T12:22:22Z"
+              },
+              "list":[
+                  {
+                      "text":"abc",
+                      "des":"des",
+                      "detail":null
+                  }
+              ],
+              "enum":[1,2,3,4],
+              "summary": {
+                "title": "Contiguous U.S., Average Temperature",
+                "units": "Degrees Fahrenheit",
+                "base_period": "1901-2000",
+                "abc":"222"
+              },
+              "data": {
+                "189512": {
+                  "value": "50.34",
+                  "anomaly": "-1.68"
+                },
+                "189612": {
+                  "value": "51.99",
+                  "anomaly": "-0.03"
+                },
+                "188712": {
+                  "value": "51.56",
+                  "anomaly": "-0.46"
+                },
+                "adf":{
+                    "aaa":"1111"
+                }
+              }
+            }
+            """;
 
         var helper = new CSharpCovertHelper();
         if (CSharpCovertHelper.CheckJson(json))
         {
             var jsonElement = JsonSerializer.Deserialize<JsonElement>(json);
+
+            helper.ToCsharpClassContent(jsonElement);
+
+            var meta = helper.JsonMetadataList;
             helper.GenerateClass(jsonElement);
+
 
             var classcodes = helper.ClassCodes;
             Console.WriteLine(classcodes.Count);
