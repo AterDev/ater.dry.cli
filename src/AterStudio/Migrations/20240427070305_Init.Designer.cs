bf44d13beacb8f6db0c69f8c45ed6c8aee584edb
niltor <zpty@outlook.com>
2024-04-28 01:15:11 +0800

    update

--- src/AterStudio/Migrations/20240427070305_Init.Designer.cs
+++ src/AterStudio/Migrations/20240427070305_Init.Designer.cs
@@ -3,16 +3,19 @@ using System;
 using Definition.EntityFramework.DBProvider;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
 namespace AterStudio.Migrations
 {
-    [DbContext(typeof(ContextBase))]
-    partial class ContextBaseModelSnapshot : ModelSnapshot
+    [DbContext(typeof(CommandDbContext))]
+    [Migration("20240427070305_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
--- src/AterStudio/Migrations/CommandDbContextModelSnapshot.cs
+++ src/AterStudio/Migrations/CommandDbContextModelSnapshot.cs
@@ -3,19 +3,16 @@ using System;
 using Definition.EntityFramework.DBProvider;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
 namespace AterStudio.Migrations
 {
-    [DbContext(typeof(ContextBase))]
-    [Migration("20240422013945_Init")]
-    partial class Init
+    [DbContext(typeof(CommandDbContext))]
+    partial class CommandDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
